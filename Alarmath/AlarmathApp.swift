afa010ab9a7ee71453a7ac3ff6b57ed06b145fbe
cirodidato <cdidato24@fed.idserve.net>
2024-12-12 15:22:46 +0100

    Initial Commit

---
+++ Alarmath/AlarmathApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmathApp.swift
+//  Alarmath
+//
+//  Created by Ciro Di Dato on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AlarmathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
